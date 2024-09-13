d9cf57cf345a908e819743b006d085e9e240cf73
gabiaabreu <gabiabr3u@gmail.com>
2024-09-13 14:43:26 -0300

    feat: adiciona swagger

---
+++ src/main/java/com/kafkaproject/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.kafkaproject.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Contact contact = new Contact();
+        contact.setEmail("gabiabr3u@gmail.com");
+        contact.setName("Gabriela Abreu");
+        contact.setUrl("https://github.com/gabiaabreu");
+
+        Info info = new Info()
+                .title("Kafka Producer-Consumer API")
+                .version("1.0")
+                .contact(contact)
+                .description("Practicing Kafka flow through an API");
+
+        return new OpenAPI().info(info);
+    }
+}
