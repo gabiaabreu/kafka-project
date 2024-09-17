bcb66c23bc3aa777b442c52fcc765e1a156ec6de
gabiaabreu <gabiabr3u@gmail.com>
2024-09-17 08:27:11 -0300

    feat: adiciona deserializer jackson

--- src/main/java/com/kafkaproject/consumer/KafkaConsumer.java
+++ src/main/java/com/kafkaproject/consumer/KafkaConsumer.java
@@ -1,5 +1,9 @@
 package com.kafkaproject.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kafkaproject.domain.entity.Mail;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +12,13 @@ public class KafkaConsumer {
 
     private final String TOPIC_NAME= "quickstart-events";
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @KafkaListener(topics = TOPIC_NAME, groupId = "1406")
-    public void consumeMessage(String message) {
-        System.out.println("Received message from topic " + TOPIC_NAME + ": " + message);
+    public void consumeMessage(String message) throws JsonProcessingException {
+        objectMapper.registerModule(new JavaTimeModule());
+
+        Mail mail = objectMapper.readValue(message, Mail.class);
+        System.out.println("Received message from topic " + TOPIC_NAME + ": " + mail);
     }
 }
