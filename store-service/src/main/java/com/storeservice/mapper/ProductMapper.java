c319d073fb54c64783ac665dd36149649fc15f2d
gabiaabreu <gabiabr3u@gmail.com>
2024-10-22 07:42:34 -0300

    feat: adiciona mapper

--- store-service/src/main/java/com/storeservice/mapper/ProductMapper.java
+++ store-service/src/main/java/com/storeservice/mapper/ProductMapper.java
@@ -4,9 +4,10 @@ import com.storeservice.domain.dto.Product;
 import com.storeservice.domain.dto.ProductCreateRequest;
 import com.storeservice.domain.entity.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductMapper {
     ProductMapper INSTANCE = Mappers.getMapper( ProductMapper.class );
 
