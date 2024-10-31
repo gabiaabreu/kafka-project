f88a624168e3294d369d8bddf9f51df7b475bc13
gabiaabreu <gabiabr3u@gmail.com>
2024-10-30 21:27:04 -0300

    feat: adiciona timezone

--- store-service/src/main/java/com/storeservice/StoreServiceApplication.java
+++ store-service/src/main/java/com/storeservice/StoreServiceApplication.java
@@ -3,10 +3,13 @@ package com.storeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class StoreServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(StoreServiceApplication.class, args);
 	}
 
