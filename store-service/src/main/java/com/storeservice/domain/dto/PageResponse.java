9b87158063d8e918f7ab3fd4b18e2d73c7c4d151
gabiaabreu <gabiabr3u@gmail.com>
2024-11-30 14:19:51 -0300

    feat: adiciona pageMetadata to pageResponse

--- store-service/src/main/java/com/storeservice/domain/dto/PageResponse.java
+++ store-service/src/main/java/com/storeservice/domain/dto/PageResponse.java
@@ -8,18 +8,27 @@ import java.util.List;
 @Data
 public class PageResponse<T> {
     private List<T> content;
-    private int pageNumber;
-    private int pageSize;
-    private long numberOfElements;
-    private long totalElements;
-    private int totalPages;
+    private PageMetadata page;
 
     public PageResponse(Page<T> page) {
         this.content = page.getContent();
-        this.pageNumber = page.getNumber();
-        this.pageSize = page.getSize();
-        this.numberOfElements = page.getNumberOfElements();
-        this.totalElements = page.getTotalElements();
-        this.totalPages = page.getTotalPages();
+        this.page = new PageMetadata(page);
+    }
+
+    @Data
+    public static class PageMetadata {
+        private int pageNumber;
+        private int pageSize;
+        private long numberOfElements;
+        private long totalElements;
+        private int totalPages;
+
+        public PageMetadata(Page<?> page) {
+            this.pageNumber = page.getNumber();
+            this.pageSize = page.getSize();
+            this.numberOfElements = page.getNumberOfElements();
+            this.totalElements = page.getTotalElements();
+            this.totalPages = page.getTotalPages();
+        }
     }
 }
